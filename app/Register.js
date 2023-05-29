2ef9fe62f83c552632258b6cca86cd12e21a9fe0
Matthew Thompkins <alkeccg@gmail.com>
2023-05-28 20:39:09 -0400

    aws implem

--- app/Register.js
+++ app/Register.js
@@ -1,6 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, TextInput, Button } from "react-native";
 import { Styles } from "./Styles";
+import AWS from 'aws-sdk';
+import { v4 as uuidv4 } from 'uuid';
+import 'react-native-get-random-values';
+
+AWS.config.update({
+  region: 'us-east-2', // Replace with your AWS region
+  accessKeyId: 'AKIATAZJI2XORXITI3PL', // Replace with your AWS access key ID
+  secretAccessKey: 'MTgonJaGYYmps3f9duyZlUb7P6tt4OfQjQvHw4Iy', // Replace with your AWS secret access key
+  endpoint:'https://dynamodb.us-east-2.amazonaws.com'
+});
+
+const documentClient = new AWS.DynamoDB.DocumentClient();
 
 export default function Register() {
   const [name, setName] = useState("");
@@ -10,10 +22,44 @@ export default function Register() {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
-  const handleRegister = () => {
-    // Implement your registration logic here
-    // Validate form fields, make API calls, etc.
-    console.log("Registering...");
+  useEffect(() => {
+    // Configure AWS SDK once when the component mounts
+    AWS.config.update({
+      region: 'us-east-2', // Replace with your AWS region
+      accessKeyId: 'AKIATAZJI2XORXITI3PL', // Replace with your AWS access key ID
+      secretAccessKey: 'MTgonJaGYYmps3f9duyZlUb7P6tt4OfQjQvHw4Iy', // Replace with your AWS secret access key
+      endpoint:'https://dynamodb.us-east-2.amazonaws.com'
+    });
+  }, []);
+
+  const handleRegister = async () => {
+    try {
+      // Generate a unique userId for the user
+      const userId = uuidv4();
+
+      // Create the user registration item
+      const item = {
+        userId,
+        name,
+        age,
+        weight,
+        email,
+        password,
+      };
+
+      // Set the parameters for the DynamoDB put operation
+      const params = {
+        TableName: 'users', // Replace 'users' with your actual table name
+        Item: item,
+      };
+
+      // Put the item into the DynamoDB table
+      await documentClient.put(params).promise();
+
+      console.log('Registration successful');
+    } catch (error) {
+      console.error('Registration failed:', error);
+    }
   };
 
   return (
